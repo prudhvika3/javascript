60431b9c785fcf01e722fd860fc9663f5a5fc27b
prudhvika3 <prudhvika369@gmail.com>
2025-02-07 15:35:17 +0530

    functions done

---
+++ my-functions.js
@@ -0,0 +1,44 @@
+// TYPES OF JAVSCRIPT FUNCTIONS
+// Author https://nagvbt.github.io/
+
+// Function Declarations
+function greet(name) {
+    return `Hello, ${name}!`;
+  }
+console.log(greet("MrNBT 123"));
+
+
+// Function Expression
+// const greet = function(name) {
+//   return `Hello, ${name}!`;
+// };
+
+// console.log(greet("FE: MrNBT")); 
+
+
+
+// Arrow Function
+// const greet = (name) => `Hello, ${name}!`;
+// console.log(greet("Balaji")); 
+
+//  Immediately Invoked Function Expressions (IIFE)
+
+// (function() {
+//     console.log("This runs right away!");
+//   })();
+
+
+
+// Callback Functions
+// const greetFn = (name) => {
+//     console.log(`Hello, ${name}!`);
+// }
+
+// function processName(callback) {
+//   const name = "Mr NBT";
+//   callback(name);
+// }
+
+// processName(greetFn); 
+
+
