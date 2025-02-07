2d4e8ccb92ca0d4445c805ef5232d22fa456a0ba
prudhvika3 <prudhvika369@gmail.com>
2025-02-07 14:33:57 +0530

    hello

---
+++ OneDrive/Desktop/java script/basic.js
@@ -0,0 +1,14 @@
+// Variables
+
+// Menu -> Run -> Run With out Debugging
+
+let message = "Hello, World!";
+const PI = 3.14;
+
+// Function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Output
+console.log(greet("Balaji")); // Hello, Alice!
